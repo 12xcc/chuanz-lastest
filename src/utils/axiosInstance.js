5f2a039175f4aecb0634f76fc3eebae777093388
xiaocan <3341385679@qq.com>
2024-11-02 20:20:44 +0800

    修改密码

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,25 +2,23 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: '', // 设置基础 URL
-    // baseURL: 'http://43.138.213.55:8080/', // 设置基础 URL
     timeout: 300000, // 设置请求超时
 });
 
 // 添加请求拦截器
 axiosInstance.interceptors.request.use(
     (config) => {
-      // 在请求头中添加 Authorization
-      const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOiJhZG1pbiIsInVzZXJfaWQiOiIxMzgiLCJleHAiOjE4MDAyNzYxODd9.m3mnq_xPvQ8GIgleyzSC-XQQCW-ZwfSJSrEqifwSdTQ"; // 永久token
-  
+      const user = JSON.parse(localStorage.getItem("user")); // 从 localStorage 获取用户数据
+      const token = user ? user.token : null; // 获取 token
+      
       if (token) {
-        // 添加 Authorization 头部
-        config.headers["Authorization"] = token; 
+        config.headers["Authorization"] = token; // 添加 Authorization 头部
       }
       return config;
     },
     (error) => {
       return Promise.reject(error);
     }
-  );
-  
-  export default axiosInstance;
+);
+
+export default axiosInstance;
